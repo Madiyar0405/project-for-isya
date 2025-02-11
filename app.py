7a1209132d6ad0443b35ee083ea54bf5c5285f61
Madiyar <123325330+Madiyar0405@users.noreply.github.com>
2025-02-11 20:54:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -86,5 +86,5 @@ def listen_page(message_id):
 #         return send_file(file_path)
 #     return jsonify({'error': 'File not found'}), 404
 if __name__ == "__main__":
-    setup_upload_directory()
+  
     app.run(host="0.0.0.0", port=5000)
